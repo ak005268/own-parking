14dbedf7969714cdd1696dae5af3fed18b97b6f7
Arunkumar K <arunsrid11@gmail.com>
2024-02-02 07:12:38 +0530

    scss moved

--- src/app/views/pages/dashboard/page.tsx
+++ src/app/views/pages/dashboard/page.tsx
@@ -1,7 +1,6 @@
 import { ReactNode, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState, exampleAction, updateNumber } from '~/store';
-import './page.scss';
 
 const Dashboard = (): ReactNode => {
     const dispatch = useDispatch();
