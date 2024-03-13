1fbf9ee31f3b32af4668421fc0507198d5f53209
Arunkumar K <arunsrid11@gmail.com>
2024-03-13 07:02:08 +0530

    logout functionalities completed

--- src/app/views/layouts/appLayout/Header.tsx
+++ src/app/views/layouts/appLayout/Header.tsx
@@ -1,24 +1,33 @@
 /* eslint-disable max-len */
 import { useSelector } from 'react-redux';
 // import { toast, Bounce } from 'react-toastify';
-import { RootState } from '~/store';
+import { RootState, clearLoginState } from '~/store';
 import { user } from '../../../../assets/images/Images';
 import { useState } from 'react';
 import { MobileHamburgerIcon } from '../../../../assets/icons/Icons';
+import { useDispatch } from 'react-redux';
 
 const Header = (props:any) => {
-    const { sidebarOpen, sidebarHandler}=props;
+    const { sidebarOpen, sidebarHandler }=props;
     const [userProfileShow, setuserProfileShow] = useState(false);
     const menu=['Profile', 'Setting', 'Subscriptions', 'Log out',];
 
     const userName=useSelector((state:RootState) => state.login);
+    const dispatch=useDispatch();
     const headerClass = sidebarOpen ? 'lg:w-[calc(100%_-_80px)]':'lg:w-[calc(100%_-_200px)] ';
+    const logout=(value:string) => {
+        if(value === 'Log out'){
+
+            dispatch(clearLoginState());
+        }
+
+    };
 
     return (
         <div className='relative'>
             <div style={{ backdropFilter: 'saturate(180%) blur(2px)' }} className={`fixed lg:px-5 px-1 flex inset-auto flex-col top-0 right-0 w-full ease-in-out duration-1000 bg-[hsla(0,0%,100%,.8)] opacity-80 left-auto ${headerClass}`}>
                 <div className=' h-[60px] flex items-center px-1 lg:px-5 bg-opacity-70'>
-                    <button type='button'  onClick={() => sidebarHandler(!sidebarOpen)} className='lg:invisible mr-2'><MobileHamburgerIcon/></button>
+                    <button type='button' onClick={() => sidebarHandler(!sidebarOpen)} className='lg:invisible mr-2'><MobileHamburgerIcon/></button>
                     <div className='w-full'> Welcome {userName?.userData?.data?.name} </div>
                     <div className='w-full'> </div>
                     <div className='relative'>
@@ -34,7 +43,7 @@ const Header = (props:any) => {
                 <div><div className='w-full h-screen z-[99] fixed ' onClick={() => setuserProfileShow(false)}></div>
                     <div className={' ease-in-out duration-500 z-[101] fixed top-14 lg:right-10 right-5 lg:w-[200px]  sm:w-20 header-outlet px-5 py-4 overflow-y-auto w-[200px] rounded-xl bg-color-white opacity-100'}>
 
-                        {menu.map((item:any) => ( <button key={`menu-${item}`} onClick={() => alert(item)} className=' hover:bg-color-orange hover:bg-opacity-20 hover:text-color-orange hover:rounded-lg w-full py-3 ease-in-out duration-300'>
+                        {menu.map((item:any) => ( <button key={`menu-${item}`} onClick={() => logout(item)} className=' hover:bg-color-orange hover:bg-opacity-20 hover:text-color-orange hover:rounded-lg w-full py-3 ease-in-out duration-300'>
                             {item}
                         </button>))}
 
